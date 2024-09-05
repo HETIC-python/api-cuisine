d6c87282eff4197a11b26dcd699fc5de3fc0cd76
SirMacCready <polocharbel@gmail.com>
2024-09-05 14:10:22 +0200

    Recipes App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Index from './index';
+import Recipes from './recipes';
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Index />}> </Route>
             <Route path="/search" element={<Index />}> </Route>
+            <Route path="/recipes" element={<Recipes />}> </Route>
+
           </Routes>
         </Router>
 </React.StrictMode>
---
+++ frontend/src/recipes.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export default function Recipes() {
+  const [recipes, setrecipes] = useState([]);
+
+  async function getRecipes() {
+    try {
+      const reponse: any = await fetch(
+        "http://localhost:3000/api/v1/all-recipes",
+        {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      const result: any = await reponse.json();
+
+      setrecipes(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {recipes?.map((e: any) => {
+          return (
+            <div>
+              <h2>{e.title}</h2>
+              <img src={e.image} alt="image" />
+              <p>{e.summary}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
